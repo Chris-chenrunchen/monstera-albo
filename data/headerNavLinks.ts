46ebc7cc0e16734a3c25eb499289840924f9074e
Chris-chenrunchen <100149266+Chris-chenrunchen@users.noreply.github.com>
2025-12-09 10:34:02 +0800

    修改propagation链接

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog/monstera-albo-care-guide', title: 'Care Guide' },
   { href: '/varieties', title: 'Varieties' },
-  { href: '/propagation', title: 'Propagation' },
+  { href: '/blog/monstera-albo-propagation', title: 'Propagation' },
   { href: '/blog', title: 'Blog' },
   { href: '#', title: 'Support', isModal: true },
 ]
